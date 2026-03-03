3e1c7436e29f586a621bcf87d81f1d33d65ee1a9
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2026-03-03 12:01:56 +0700

    fix error log

--- api-application/app/core/logger.py
+++ api-application/app/core/logger.py
@@ -18,7 +18,7 @@ handler = RotatingFileHandler(
 )
 
 formatter = logging.Formatter(
-    "%(asctime)s | %(levelname)s | %(message)s"
+    "%(asctime)s --> %(levelname)s --> %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
 )
 
 handler.setFormatter(formatter)
